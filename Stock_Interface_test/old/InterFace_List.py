de66276419c19ba14f3076cb810196d94c563880
张海南 <wt52571314@126.com>
2016-09-01 11:37:10 +0800

    Update InterFace_List.py
    脱敏

--- Stock_Interface_test/old/InterFace_List.py
+++ Stock_Interface_test/old/InterFace_List.py
@@ -5,15 +5,14 @@
 # 积木股票接口List
 # ===============================================================================
 List_interface = [\
-'https://stock-api.jimustock.com/api/v1/trade/asset?stockType=1&usAccountID=66', \
-'https://stock-api.jimustock.com/api/v1/us/account/getCouponView?usAccountID=66', \
-'https://stock-api.jimustock.com/api/v1/trade/positions?paged=false&skip=0&take=5&usAccountID=66', \
-'https://stock-api.jimustock.com/api/v1/info?skip=0&take=5&type=main', \
-'https://stock-api.jimustock.com/api/v1/info?skip=0&take=5&type=custom', \
-'https://stock-api.jimustock.com/api/v1/market/getIncreaseList?skip=0&stockType=4&take=5', \
-'https://stock-api.jimustock.com/api/v1/market/getTimeTrend?days=1&exchangeCode=XNYS&period=1&stockType=1&symbol=BABA', \
-]
-user_name = '18070505645'
-pwd = 'gx52571314.com'
-url_result = 'https://stock-api.jimustock.com/api/v1/security/magicCode'
+'https://stock-api.jimustock.csdom/apids/v/*********', \/assset?stos/*********', \sAccountID=66', \
+'https://stock-api.jimustock.csdom/apids/v1//*********', \', \
+'https://stock-api.jimustock.csdom/apids/v1/*********', \
+'https://stock-api.jimustock.csdom/apids/v1/*********', \', \
+'https://stock-api.jimustock.csdom/apids/v1//*********', \', \
+'https://stock-api.jimustock.csdom/apids/v1/market/g/*********', \ist?skip=0&stockType=4&take=5', \
+'https://stock-api.jimustock.csdom/apids/v1/marke/*********', \end?days=1&exchangdeC/*********', \
+user_name = 'wq we'
+pwd = 'hdhhdhd'
+url_result = 'https://stock-api.jimustock.com/*********', \/CZ'
 List = []
